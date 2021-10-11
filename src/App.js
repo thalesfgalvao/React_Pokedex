a3769ec1ceb15b476eaa1d89100561ec12ff52b0
Thales Fernando <thalesf.galvao@gmail.com>
2021-10-11 08:18:33 -0300

    Feat: capitalize first letter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
-import api from './services/api'
-
-
+import MiniPostCards from './Components/MiniPostCards';
+import api from './services/api';
 
 const App = () => {
-  const [pokemon, setPokemon] = useState('');
+  const [typedPokemon, setTypedPokemon] = useState('');
+  const [pokemon, setPokemon] = useState();
   const [error, setError] = useState('');
+
+
 
   const handleClick = async () => {
     try{
-      const response = await api.get(`/pokemon/${pokemon}`);
+      const response = await api.get(`/pokemon/${typedPokemon}`);
+
       console.log(response.data);
+      setPokemon(response.data);
+
     }catch(error){
       console.log(error.message);
       setError(error.message);
@@ -26,12 +31,14 @@ const App = () => {
         <h1>Welcome to Pokedex!</h1>
         <div className="form-group">
           <input
-            onChange={(e) => setPokemon(e.target.value.toLowerCase())}
+            onChange={(e) => setTypedPokemon(e.target.value.toLowerCase())}
             placeholder="Type a pokemon"/>
           <button type="submit" onClick={handleClick}>Search</button>
             {error && <p>{error}</p>}
           </div>
+          {pokemon && <MiniPostCards pokemonName={pokemon.name} pokemonImage={pokemon.sprites.front_default}/>}
         </div>
+      
       </header>
     </div>
   );
---
+++ src/Components/MiniPostCards/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+import {capitalizeFirstLetter} from '../../Utils/capitalizeFirstLetter';
+
+const MiniPostCards = ({pokemonName, pokemonImage}) => {
+    
+    return(
+        <div className="miniPostCards">
+            <div className="cardBody" style={{ width: '18rem' }}>
+                <div className="bodyContent">
+                    <div className="pokemonTitle">
+                        <p>{capitalizeFirstLetter(pokemonName)}</p>
+                    </div>
+                    <div className="pokemonSprite">
+                        <img src={pokemonImage} alt="Pokemon_Image"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MiniPostCards;
