dc9d6e96d138189a5595c7577be72ae5abeda42a
Thales Fernando <thalesf.galvao@gmail.com>
2021-10-14 07:46:29 -0300

    Fix: add blue and red balls in background

---
+++ src/Components/Balls/Balls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Balls = () => {
+    return(
+        <div className="Balls">
+            <div className="redBall"/>
+            <div className="blueBall"/>
+        </div>
+    )
+}
+
+export default Balls;
