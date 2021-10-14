3304715b726ba59e09b1ff4fd58889d9286a33ed
Thales Fernando <thalesf.galvao@gmail.com>
2021-10-14 07:45:50 -0300

    Feat: add initial file

---
+++ src/Components/PostCards/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const PostCard = () => {
+    return(
+        <div className="PostCards">
+            
+        </div>
+    )
+}
+
+export default PostCard;
