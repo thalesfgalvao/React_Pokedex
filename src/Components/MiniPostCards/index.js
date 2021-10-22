85589f4c8c4724e48d56fbd58c0096f4c04485bd
Thales Fernando <thalesf.galvao@gmail.com>
2021-10-22 09:25:21 -0300

    Feat: add link to redirect to another page

--- src/Components/MiniPostCards/index.js
+++ src/Components/MiniPostCards/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 import {capitalizeFirstLetter} from '../../Utils/capitalizeFirstLetter';
 
 const MiniPostCards = ({pokemonName, pokemonImage}) => {
+  
     
     return(
-        <div className="miniPostCards">
-            <div className="cardBody">
-                <div className="bodyContent">
-                    <div className="pokemonTitle">
-                        <p>{capitalizeFirstLetter(pokemonName)}</p>
-                    </div>
-                    <div className="pokemonSprite">
-                        <img src={pokemonImage} alt="Pokemon_Image"/>
+        <Link to={`/PostCardsPage/${pokemonName}`}>
+            <div className="miniPostCards">
+                <div className="cardBody">
+                    <div className="bodyContent">
+                        <div className="pokemonTitle">
+                            <p>{capitalizeFirstLetter(pokemonName)}</p>
+                        </div>
+                        <div className="pokemonSprite">
+                            <img className="pokemonImage" src={pokemonImage} alt="Pokemon_Image"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/Components/PostCards/index.js
+++ src/Components/PostCards/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './style.css';
+import { useParams } from 'react-router';
+import api from '../../services/api';
 
-const PostCard = () => {
+export const PostCards = ({pokemonName}) => {
+    
+    const [pokemon, setPokemon] = useState(''); 
+
+    useEffect(() => {    
+        try{
+            const response = api.get(`/pokemon/${pokemonName}`);
+            console.log(response.data);
+            setPokemon(response.data);
+        }catch(error){
+            console.log(error.message);
+        }
+
+    });
+
+    const params = useParams();
+    
+    console.log(params);
+
     return(
         <div className="PostCards">
-            
+            <h1 pokemonName={`${pokemon.name}`}>{params}</h1>
         </div>
     )
 }
-
-export default PostCard;
