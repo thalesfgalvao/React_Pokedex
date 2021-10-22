eda731c5d61ce278545f22ed814a468b46e33051
Thales Fernando <thalesf.galvao@gmail.com>
2021-10-22 09:23:36 -0300

    Feat: add new pag called Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useState } from 'react';
+import MiniPostCards from '../../Components/MiniPostCards';
+import api from '../../services/api';
+import Balls from '../../Components/Balls/Balls';
+
+export const Home = () => {
+    const [typedPokemon, setTypedPokemon] = useState('');
+    const [pokemon, setPokemon] = useState();
+    const [error, setError] = useState('');
+
+  const handleClick = async () => {
+    try{
+      const response = await api.get(`/pokemon/${typedPokemon}`);
+
+      console.log(response.data);
+      setPokemon(response.data);
+
+    }catch(error){
+      console.log(error.message);
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Balls/>
+       <div className="container">
+        <h1>Welcome to Pokedex!</h1>
+        <div className="form-group">
+          <input
+            onChange={(e) => setTypedPokemon(e.target.value.toLowerCase())}
+            placeholder="Type a pokemon"/>
+          <button type="submit" onClick={handleClick}>Search</button>
+            {error && <p>{error}</p>}
+          </div>
+          {pokemon && <MiniPostCards pokemonName={pokemon.name} pokemonImage={pokemon.sprites.front_default}/>}
+        </div>
+      </header>
+    </div>
+  );
+}
---
+++ src/pages/PostCardsPage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { PostCards } from "../../Components/PostCards/index";
+
+export const PostCardsPage = () =>{
+    return(
+        <div>
+            <PostCards/>
+        </div>
+    );
+}
